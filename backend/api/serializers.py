6312d2b401eefa0d3a8f2cb441273011dd58da56
Kensei <tehkensei@gmail.com>
2025-03-22 23:25:34 -0500

    added health data, fitness, logs to serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
 from top3health.models import *
 
+# Serializer for Myhealthscreening model
 class healthScreeningSerializer(serializers.ModelSerializer):
     class Meta:
         model = Myhealthscreening
         fields = '__all__'
 
+# Serializer for Myfitness model
 class fitnessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Myfitness
         fields = '__all__'
 
+# Serializer for Mydailylog model
 class dailyLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mydailylog
         fields = '__all__'
 
+# Serializer for CustomUser model, including nested related models
 class userSerializer(serializers.ModelSerializer):
-    health_screening = healthScreeningSerializer(many=True, read_only=True)  # Include health screening data
+    # Nested serializers for health screening, fitness, and daily log
+    health_screening = healthScreeningSerializer(many=True, read_only=True)
     fitness = fitnessSerializer(many=True, read_only=True)
     daily_log = dailyLogSerializer(many=True, read_only=True)
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from top3health.models import CustomUser
-from .serializers import userSerializer
+from top3health.models import CustomUser, Myhealthscreening, Mydailylog, Myfitness
+from .serializers import userSerializer, healthScreeningSerializer, fitnessSerializer, dailyLogSerializer
 
 class User(APIView):
     def get(self, request, *args, **kwargs):
         user_id = kwargs.get('user_id')
+        
+        # Get the user instance
         user_instance = get_object_or_404(CustomUser, id=user_id)
-        serializer = userSerializer(user_instance)
-        return Response(serializer.data)
+        
+        # Fetch related data (health_screening, daily_log, fitness)
+        health_screening_data = Myhealthscreening.objects.filter(customuser=user_instance)
+        daily_log_data = Mydailylog.objects.filter(customuser=user_instance)
+        fitness_data = Myfitness.objects.filter(customuser=user_instance).first()  # Only one instance per user
+        
+        # Serialize the user and related data
+        user_data = userSerializer(user_instance).data
+        health_screening_serialized = healthScreeningSerializer(health_screening_data, many=True).data
+        daily_log_serialized = dailyLogSerializer(daily_log_data, many=True).data
+        fitness_serialized = fitnessSerializer(fitness_data).data if fitness_data else None
+
+        # Add related data to the user data response
+        user_data['health_screening'] = health_screening_serialized
+        user_data['fitness'] = fitness_serialized
+        user_data['daily_log'] = daily_log_serialized
+
+        return Response(user_data)
 
     def post(self, request, *args, **kwargs):
         serializer = userSerializer(data=request.data)
